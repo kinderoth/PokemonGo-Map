b04aacc34e9e0495743f5eaf8e9477171a8a0dfb
Jerrod <jlankford@salesforce.com>
2016-07-24 02:41:23 -0400

    added list view to pokemon map

--- static/js/app.js
+++ static/js/app.js
@@ -34,6 +34,11 @@
 		$navToggle = document.querySelector('a[href="#nav"]'),
 		$navClose;
 
+	// List
+	var $list = document.querySelector('#list'),
+		$listToggle = document.querySelector('a[href="#list"]'),
+		$listClose;
+
 	var $overlay = document.querySelector('#trigger-overlay');
 
 	// Event: Prevent clicks/taps inside the nav from bubbling.
@@ -41,9 +46,14 @@
 		event.stopPropagation();
 	});
 
+	addEventsListener($list, 'click touchend', function(event){
+		event.stopPropagation();
+	});
+
 	// Event: Hide nav on body click/tap.
 	addEventsListener($body, 'click touchend', function(event) {
 		$nav.classList.remove('visible');
+		$list.classList.remove('visible');
 	});
 
 	// Toggle.
@@ -53,6 +63,14 @@
 		event.preventDefault();
 		event.stopPropagation();
 		$nav.classList.toggle('visible');
+		$list.classList.remove('visible');
+	});
+
+	$listToggle.addEventListener('click', function(event){
+		event.preventDefault();
+		event.stopPropagation();
+		$list.classList.toggle('visible');
+		$nav.classList.remove('visible');
 	});
 
 	// Close.
@@ -64,10 +82,18 @@
 	$navClose.tabIndex = 0;
 	$nav.appendChild($navClose);
 
+	$listClose = document.createElement('a');
+	$listClose.href = '#';
+	$listClose.className = 'close';
+	$listClose.tabIndex = 0;
+	$list.appendChild($listClose);
+
 	// Event: Hide on ESC.
 	window.addEventListener('keydown', function(event) {
-		if (event.keyCode == 27)
+		if (event.keyCode == 27) {
 			$nav.classList.remove('visible');
+			$list.classList.remove('visible');
+		}
 	});
 
 	// Event: Hide nav on click.
@@ -76,5 +102,11 @@
 		event.stopPropagation();
 		$nav.classList.remove('visible');
 	});
+
+	$listClose.addEventListener('click', function(event){
+		event.preventDefault();
+		event.stopPropagation();
+		$list.classList.remove('visible');
+	})
 
 })();
