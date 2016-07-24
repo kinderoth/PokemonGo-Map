5fd50db498b211dd9bd0e698ca97457cae04e5fb
Jerrod <jlankford@salesforce.com>
2016-07-24 19:26:57 -0400

    Merges

--- static/js/app.js
+++ static/js/app.js
@@ -34,14 +34,24 @@
 		$navToggle = document.querySelector('a[href="#nav"]'),
 		$navClose;
 
+	// List
+	var $list = document.querySelector('#list'),
+		$listToggle = document.querySelector('a[href="#list"]'),
+		$listClose;
+
 	// Event: Prevent clicks/taps inside the nav from bubbling.
 	addEventsListener($nav, 'click touchend', function(event) {
 		event.stopPropagation();
 	});
 
+	addEventsListener($list, 'click touchend', function(event){
+ 		event.stopPropagation();
+ 	});
+
 	// Event: Hide nav on body click/tap.
 	addEventsListener($body, 'click touchend', function(event) {
 		$nav.classList.remove('visible');
+		$list.classList.remove('visible');
 	});
 
 	// Toggle.
@@ -51,7 +61,15 @@
 		event.preventDefault();
 		event.stopPropagation();
 		$nav.classList.toggle('visible');
+		$list.classList.remove('visible');
 	});
+
+	$listToggle.addEventListener('click', function(event){
+ 		event.preventDefault();
+ 		event.stopPropagation();
+ 		$list.classList.toggle('visible');
+ 		$nav.classList.remove('visible');
+  });
 
 	// Close.
 
@@ -62,10 +80,18 @@
 	$navClose.tabIndex = 0;
 	$nav.appendChild($navClose);
 
+	$listClose = document.createElement('a');
+  $listClose.href = '#';
+  $listClose.className = 'close';
+  $listClose.tabIndex = 0;
+  $list.appendChild($listClose);
+
 	// Event: Hide on ESC.
 	window.addEventListener('keydown', function(event) {
-		if (event.keyCode == 27)
+		if (event.keyCode == 27) {
 			$nav.classList.remove('visible');
+			$list.classList.remove('visible');
+		}
 	});
 
 	// Event: Hide nav on click.
@@ -74,5 +100,11 @@
 		event.stopPropagation();
 		$nav.classList.remove('visible');
 	});
+
+	$listClose.addEventListener('click', function(event){
+ 			event.preventDefault();
+ 			event.stopPropagation();
+ 			$list.classList.remove('visible');
+ 	})
 
 })();
